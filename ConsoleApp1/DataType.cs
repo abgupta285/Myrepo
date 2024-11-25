f5ffd44c1ec6872e82d5e0ed461fe0a3ced27812
abgupta285 <abhijeet.gupta285@gmail.com>
2024-11-25 16:05:11 -0600

    My First commit Version1  with file DataType.cs

---
+++ ConsoleApp1/DataType.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+   /* internal class DataType
+    {
+        static void Main(String[] args)
+        {
+            
+            int a = 10;
+            a = 20;
+            Console.WriteLine("Value of Integer the variables are:"+a);
+            bool b = true;
+            b = false;
+            Console.WriteLine("Value of boolean the variables are:" +b);
+            int? i=null;
+            
+
+
+        }
+       
+   
+
+
+
+
+    }*/
+}
